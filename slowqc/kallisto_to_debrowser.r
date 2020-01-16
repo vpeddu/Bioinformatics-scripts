6e81981bb52cf1190a406c9c9feacc89caa41082
Vikas Peddu <Vpeddu@uw.edu>
2020-01-16 13:35:58 -0800

    Update kallisto_to_debrowser.r

--- slowqc/kallisto_to_debrowser.r
+++ slowqc/kallisto_to_debrowser.r
@@ -1,6 +1,5 @@
 # Script to go from folder of Kallisto abundance files to DEBrowser ready counts input
 # Arg 1 = file path of folder containing Kallisto output 
-# Arg 2 = Tximport style classifications file
 library("debrowser")
 library("Rsamtools")
 library("Rsubread")
